35eb134ea1026c4ee79af5c1d6947cc060f56fdc
Shatti <lg22416@bristol.ac.uk>
2025-04-11 09:42:32 +0100

    adding go imports on saves

--- init.lua
+++ init.lua
@@ -641,7 +641,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        --go = { { 'gofmt' } },
+        go = { 'goimports' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
