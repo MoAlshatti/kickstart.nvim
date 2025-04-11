80e42fb5c28e47618815ab4fa79aca5d15b4987e
Shatti <lg22416@bristol.ac.uk>
2025-04-11 15:27:23 +0100

    adding vim-be-good game as a plugin

--- init.lua
+++ init.lua
@@ -188,6 +188,10 @@ require('lazy').setup({
     },
   },
 
+  {
+    'ThePrimeagen/vim-be-good',
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
